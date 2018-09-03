885c09b29f3aa2c9cdd67109192fdd82b4d11fdc
AlicanInsider <alican.camlibel@useinsider.com>
2018-09-03 19:04:28 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+$('.dashboard-sidebar-rectangle').click(function () {
+    $('.dashboard-content-sidebar').toggleClass('collapsed');
+    $('.dashboard-content-body').toggleClass('streched');
+    $('.dashboard-sidebar-rectangle').toggleClass('collapsed');
+});
